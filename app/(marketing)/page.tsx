6d7becad8cd46a1a461027a8ae52803e8346ec86
Your Name <you@example.com>
2025-01-12 05:33:33 -0500

    duoleet bird

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -6,14 +6,15 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <div>
+      <div className = "flex justify-center items-center ">
       <Image 
-            src="/images/duolingo-eagle.svg" 
-            width={32} 
-            height={40}
+            src="/images/duoleet-eagle.png" 
+            width={150} 
+            height={180}
             alt="Duoleet Eagle"
-            className="font-feather mr-4 rounded-md flex flex-col" 
-          />
-      <div className=" font-feather flex flex-col items-center gap-y-8">
+            className="font-feather mr-4 rounded-md flex flex-colblock transform translate-x-[-30px] translate-y-[-20px]">
+              </Image></div> 
+        <div className=" font-feather flex flex-col items-center gap-y-8">
         <h1 className="p-2 font-feather lg: text-2xl font-bold text-neutral-600 max-w-[480px] text-center">
           Learn, practice, and master new concepts and languages with Duoleet.
         </h1>
@@ -21,7 +22,7 @@ export default function Home() {
 
       {/* Wrap the Link component around the Button component */}
       <Link href="/dropdownmenu">
-        <Button size="lg" variant="secondary" className="font-feather w-full bg-yellow-500 border-yellow-700 hover:bg-yellow-700">
+        <Button size="lg" variant="secondary" className=" mb-4 font-feather w-full bg-yellow-500 border-yellow-700 hover:bg-yellow-700">
           Get Started
         </Button>
       </Link>
