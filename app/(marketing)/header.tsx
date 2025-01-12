253bc29b68c3d1eb7af9c1267bbe6f9b49b67ef1
Your Name <you@example.com>
2025-01-11 19:19:36 -0500

    added fonts and startup page

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -1,7 +1,14 @@
 export const Header = () => {
   return (
-    <header className="h-20 w-full border-b-2 border-slate-200 px-4">
-      Header
-    </header>
+    <div>
+      <header className="h-20 w-full border-b-2 border-slate-200 px-4">
+         <h1 className=" font-feather text-2xl font-extrabold text-green-600 tracking-wide">
+          Duo-leet
+        </h1>
+      </header>
+      <div>
+        
+      </div>
+    </div>
   );
 };
